58aa8915f3b7f6753520a700f250e70781840c2f
IfyN <ifynwosu0@gmail.com>
2024-04-07 20:36:42 +0100

    add data object

---
+++ data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    id: 0,
+    image: "/assets/hotel-image-1.png",
+    name: "Iberostar Grand Salome",
+    location: "Costa Adeje, Tenerife",
+    rating: 5,
+    adult: 2,
+    children: 2,
+    infant: 1,
+    date: "3rd July, 2019 for 7 days",
+    departure: "East Midlands",
+    price: "£1136.50",
+    description:
+      "The Iberostar Grand Salome has en exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf de Sur and the Amarillo Gold Courses.",
+  },
+  {
+    id: 1,
+    image: "/assets/hotel-image-1.png",
+    name: "Aguamarina Golf Hotel",
+    location: "Costa Adeje, Tenerife",
+    rating: 4,
+    adult: 2,
+    children: 1,
+    date: "27th May, 2019 for 7 days",
+    departure: "Liverpool",
+    price: "£696.80",
+    description:
+      "The Iberostar Grand Salome has en exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf de Sur and the Amarillo Gold Courses.",
+  },
+  {
+    id: 2,
+    image: "/assets/hotel-image-3.png",
+    name: "Las Piramides Resort",
+    location: "Costa Adeje, Tenerife",
+    rating: 3,
+    adult: 2,
+    children: 2,
+    date: "3rd July, 2019 for 7 days",
+    departure: "Manchester",
+    price: "£499.99",
+    description:
+      "The Iberostar Grand Salome has en exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf de Sur and the Amarillo Gold Courses.",
+  },
+];
